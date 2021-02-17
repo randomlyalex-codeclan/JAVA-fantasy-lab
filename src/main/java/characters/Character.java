4140c33260ac3f04820fb9f689491953f8347e33
Alex Dodd <alexdodd@gmail.com>
2021-02-17 22:19:54 +0000

    updated this to be an overall abstract class, and allow NPC and enemies to use it.

--- src/main/java/characters/Character.java
+++ src/main/java/characters/Character.java
@@ -1,43 +1,39 @@
 package characters;
 
+import behaviours.IAttack;
+import behaviours.IDefend;
 import items.Item;
-import items.Weapon;
-
-import java.util.ArrayList;
 
 public abstract class Character {
 
     private String name;
     private int currentHealth;
-    private ArrayList<Item> inventory;
-    private int experience;
+    private int powerRating;
+    private int defenceRating;
 
-    public Character(String name, int startingHealth){
+    public Character(String name, int startingHealth, int startingPowerRating, int startingdefenceRating) {
         this.name = name;
         this.currentHealth = startingHealth;
-        this.inventory = new ArrayList<Item>();
-        this.experience = 0;
+        this.powerRating = startingPowerRating;
+        this.defenceRating = startingdefenceRating;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public int getCurrentHealth() {
-        return currentHealth;
+        return this.currentHealth;
     }
 
-    public ArrayList<Item> getInventory() {
-        return inventory;
+    public int getPowerRating() {
+        return this.powerRating;
     }
 
-    public int getExperience() {
-        return experience;
+    public int getDefenceRating() {
+        return defenceRating;
     }
 
-    public void addToInventory(Item item){
-        this.inventory.add(item);
-    }
+
+
 }
-
-
