7d2107be22979343144d1b9d5939c1776dfdb83f
Alex Dodd <alexdodd@gmail.com>
2021-02-22 00:34:35 +0000

    random dice generator with roll method is easy, the other parts will use this.
    Not sure if it should keep a list of rolls and a Class variable?

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -4,6 +4,8 @@ public class Dice {
     private final Random rand;
     private final int faces;
 
+    //Maybe have a class static variable as a history of rolls?
+
     public Dice(int faces) {
         this.rand = new Random();
         this.faces = faces;
